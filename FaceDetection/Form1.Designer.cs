c71a0323b953617f2f6b6e4f6ce2a62b278d3e1d
Ahmet Batuhan Yılmaz <59421016+lashraelrx@users.noreply.github.com>
2021-09-03 14:46:22 +0300

    FaceIdDetection

---
+++ FaceDetection/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace FaceDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idButton = new System.Windows.Forms.Button();
+            this.faceButton = new System.Windows.Forms.Button();
+            this.folderButton = new System.Windows.Forms.Button();
+            this.folderBox = new System.Windows.Forms.TextBox();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.successCount = new System.Windows.Forms.Label();
+            this.failCount = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // idButton
+            // 
+            this.idButton.Location = new System.Drawing.Point(12, 7);
+            this.idButton.Name = "idButton";
+            this.idButton.Size = new System.Drawing.Size(162, 186);
+            this.idButton.TabIndex = 0;
+            this.idButton.Text = "ID Detection Mode";
+            this.idButton.UseVisualStyleBackColor = true;
+            this.idButton.Click += new System.EventHandler(this.idButton_Click);
+            // 
+            // faceButton
+            // 
+            this.faceButton.Location = new System.Drawing.Point(12, 205);
+            this.faceButton.Name = "faceButton";
+            this.faceButton.Size = new System.Drawing.Size(162, 186);
+            this.faceButton.TabIndex = 1;
+            this.faceButton.Text = "Face Detection Mode";
+            this.faceButton.UseVisualStyleBackColor = true;
+            this.faceButton.Click += new System.EventHandler(this.faceButton_Click);
+            // 
+            // folderButton
+            // 
+            this.folderButton.Location = new System.Drawing.Point(12, 415);
+            this.folderButton.Name = "folderButton";
+            this.folderButton.Size = new System.Drawing.Size(110, 23);
+            this.folderButton.TabIndex = 2;
+            this.folderButton.Text = "Select Folder";
+            this.folderButton.UseVisualStyleBackColor = true;
+            this.folderButton.Click += new System.EventHandler(this.folderButton_Click);
+            // 
+            // folderBox
+            // 
+            this.folderBox.Enabled = false;
+            this.folderBox.Location = new System.Drawing.Point(128, 418);
+            this.folderBox.Name = "folderBox";
+            this.folderBox.Size = new System.Drawing.Size(498, 20);
+            this.folderBox.TabIndex = 3;
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(180, 7);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(446, 384);
+            this.logBox.TabIndex = 4;
+            this.logBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(440, 398);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Success:";
+            // 
+            // successCount
+            // 
+            this.successCount.AutoSize = true;
+            this.successCount.Location = new System.Drawing.Point(497, 398);
+            this.successCount.Name = "successCount";
+            this.successCount.Size = new System.Drawing.Size(13, 13);
+            this.successCount.TabIndex = 6;
+            this.successCount.Text = "0";
+            // 
+            // failCount
+            // 
+            this.failCount.AutoSize = true;
+            this.failCount.Location = new System.Drawing.Point(566, 398);
+            this.failCount.Name = "failCount";
+            this.failCount.Size = new System.Drawing.Size(13, 13);
+            this.failCount.TabIndex = 8;
+            this.failCount.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(534, 398);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fail:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 448);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(110, 30);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(653, 7);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(347, 384);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 490);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.failCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.successCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.folderBox);
+            this.Controls.Add(this.folderButton);
+            this.Controls.Add(this.faceButton);
+            this.Controls.Add(this.idButton);
+            this.Name = "Form1";
+            this.Text = "Jarvis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button idButton;
+        private System.Windows.Forms.Button faceButton;
+        private System.Windows.Forms.Button folderButton;
+        private System.Windows.Forms.TextBox folderBox;
+        private System.Windows.Forms.RichTextBox logBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label successCount;
+        private System.Windows.Forms.Label failCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ FaceDetection/Form1.cs
@@ -0,0 +1,184 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using Tesseract;
+namespace FaceDetection
+{
+    public partial class Form1 : Form
+    {
+                    
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_default.xml");
+        public string FolderPath;
+        public int failCountG = 0;
+        public int successCountG = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Stopwatch watch = new Stopwatch();
+        
+
+
+        private void folderButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if(result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    FolderPath = fbd.SelectedPath;
+                    folderBox.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void idButton_Click(object sender, EventArgs e)
+        {
+            var Photos2 = GatherPhotos();
+
+            if(string.IsNullOrWhiteSpace(FolderPath))
+            {
+                MessageBox.Show("You must select a folder first.");
+            }
+            for (var i = 0; i < Photos2.Count; i++)
+            {
+                ProcessId(Photos2[i]);
+            }
+        }
+
+        private void faceButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(FolderPath))
+            {
+                MessageBox.Show("You must select a folder first.");
+            }
+            var Photos = GatherPhotos();
+            if(Photos == null)
+            {
+                return;
+            }
+            for (var i = 0; i < Photos.Count; i++)
+            {
+                ProcessPhoto(Photos[i]);
+            }
+        }
+
+        private void ProcessPhoto(String photopath)
+        {
+
+                
+                Bitmap img = new Bitmap(Image.FromFile(photopath));
+                BitmapData bitmapData = img.LockBits(new Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadOnly, img.PixelFormat);
+                img.UnlockBits(bitmapData);
+                
+                Image<Bgr, byte> grayImage = new Image<Bgr, byte>(img);
+                Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(image: grayImage, scaleFactor: 1.2, minNeighbors: 1);
+                if (rectangles.Count().ToString() == "0")
+                {
+
+                    failCountG += 1;
+                    UpdateLabels(failCountG.ToString(), false);
+                    img = ResizeBitmap(img, 50, 50);
+                    Clipboard.SetDataObject(img);
+ 
+                    DataFormats.Format image = DataFormats.GetFormat(DataFormats.Bitmap);
+                    logBox.Paste(image);
+                
+                }
+                else
+                {
+                    successCountG += 1;
+                    double rectanglesWidth = (rectangles[0].Width * rectangles[0].Height);
+
+                    Console.WriteLine(rectanglesWidth.ToString());
+                    double imageWidth = img.Width * img.Height;
+                    Console.WriteLine(imageWidth.ToString());
+                    double calc = (rectanglesWidth / imageWidth) * 100;
+                    Console.WriteLine(calc.ToString());
+                    UpdateLabels(successCountG.ToString(), true);
+                }
+                listBox1.Items.Add(img.Width+img.Height);
+
+
+        }
+        private void ProcessId(String photopath)
+        {
+
+            Bitmap img = new Bitmap(Image.FromFile(photopath));
+            var ocr = new TesseractEngine("./tessdata","eng");
+            var result = ocr.Process(img);
+            richTextBox1.Text = result.GetText();
+        }
+
+        private Bitmap ResizeBitmap(Bitmap sourceBMP, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                g.DrawImage(sourceBMP, 0, 0, width, height);
+            }
+            return result;
+        }
+
+        private void UpdateLabels(String number, Boolean success)
+        {
+            if (success)
+            {
+                successCount.Text = number;
+
+            } else
+            {
+                failCount.Text = number;
+            }
+
+        }
+        private List<string> GatherPhotos()
+        {
+            try
+            {
+                var files = Directory.GetFiles(@"" + FolderPath, "*.*", SearchOption.TopDirectoryOnly);
+                List<string> imageFiles = new List<string>();
+                foreach (string filename in files)
+                {
+                    if (Regex.IsMatch(filename, @"\.jpg$|\.jpeg$"))
+                    {
+                        imageFiles.Add(filename);
+                    }
+                }
+                return imageFiles;
+            } 
+            catch
+            {
+                return null;
+            }
+        }
+        private void Append(String text)
+        {
+            logBox.Text += "\n" + text;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+   
+
+    
+}
+
